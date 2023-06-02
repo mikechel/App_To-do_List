5ddfd5b076915135ad3345e5a2a8fc991a109440
mikechel <michekmi@hotmail.com>
2023-06-02 20:51:47 -0300

    Primeiro Commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: ['@babel/polyfill', path.resolve(__dirname, './src/main.js')],
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.js.map',
+  },
+  devServer: {
+    // contentBase
+    static : {
+      directory : path.join(__dirname, "./public")
+    },
+    port: 3000,
+    // publicPath
+    devMiddleware:{
+       publicPath: "https://localhost:3000/",
+    },
+    // hotOnly
+    hot: "only",
+  },
+  devtool: 'eval-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+  ],
+}
